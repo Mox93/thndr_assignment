fe67d237b0ab45d8ae5132d73bb2e701e65d22a3
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-07-16 16:47:10 +0200

    Created db class OwnedStockDB

--- app/db/stock.py
+++ app/db/stock.py
@@ -28,4 +28,21 @@ class StockPriceDB(BaseDB):
                 price real NOT NULL,
                 availability integer NOT NULL
             );
-            """)
+""")
+
+
+class OwnedStockDB(BaseDB):
+    __table_name__ = "owned_stock"
+
+    @classmethod
+    async def create(cls):
+        async with cls.__pool__.acquire() as conn:
+            await conn.execute(f"""
+            CREATE TABLE IF NOT EXISTS {cls.__table_name__}(
+                user_id uuid REFERENCES "{UserDB.__table_name__}"(id) NOT NULL,
+                stock_id uuid REFERENCES {StockDB.__table_name__}(id) NOT NULL,
+                name varchar(256),
+                total integer NOT NULL,
+                UNIQUE(user_id, stock_id)
+            );
+""")
