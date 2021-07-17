df3d0d208b7ccdeb6fb8a7839f984412dc6552ed
mox93 <mohamed.ragaiy.saleh@gmail.com>
2021-07-17 02:26:52 +0200

    Implemented add/remove_funds functions

--- app/api/funds.py
+++ app/api/funds.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter
 
 from ..models.actions import Deposit, Withdraw
+from ..models.user import User
+from ..utils.funds import add_funds, remove_funds
 
 
 router = APIRouter(tags=["Funds"])
 
 
-@router.post("/deposit")
-async def deposit(info: Deposit):
-    pass
+@router.post("/deposit", response_model=User)
+async def deposit(info: Deposit) -> User:
+    return await add_funds(info.user_id, info.amount)
 
 
-@router.post("/withdraw")
-async def withdraw(info: Withdraw):
-    pass
+@router.post("/withdraw", response_model=User)
+async def withdraw(info: Withdraw) -> User:
+    return await remove_funds(info.user_id, info.amount)
---
+++ app/utils/funds.py
@@ -0,0 +1,43 @@
+from uuid import UUID
+
+from fastapi import HTTPException
+
+from .user import get_user
+from ..db.user import UserDB
+from ..models.user import User, UserInDB
+
+
+async def add_funds(user_id: UUID, amount: float) -> User:
+    user = await get_user(user_id)
+    user_db = await UserDB.update(
+        {"balance": user.balance + amount},
+        id=user_id
+    )
+    user_db = UserInDB.parse_obj(user_db[0])
+
+    return User(
+        user_id=user_db.id,
+        name=user_db.name,
+        balance=user_db.balance,
+        stock=user.stock
+    )
+
+
+async def remove_funds(user_id: UUID, amount: float) -> User:
+    user = await get_user(user_id)
+
+    if amount > user.balance:
+        raise HTTPException(400, "Not Enough Balance")
+
+    user_db = await UserDB.update(
+        {"balance": user.balance - amount},
+        id=user_id
+    )
+    user_db = UserInDB.parse_obj(user_db[0])
+
+    return User(
+        user_id=user_db.id,
+        name=user_db.name,
+        balance=user_db.balance,
+        stock=user.stock
+    )
